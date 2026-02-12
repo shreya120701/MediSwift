da278d74a1446a50b038f6bfe4eb086416be7cff
shreya <501051@btc.com.bh>
2026-02-12 10:36:49 +0530

    adding directory and code

---
+++ ekart/app/src/main/java/com/med/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.med.shopping.Prevalent;
+
+import com.med.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
